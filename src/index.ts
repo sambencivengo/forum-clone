e1f151aa4cd1dc1e6892df4f8fc5cd211cf18a09
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 16:23:35 -0700

    server started

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
 
+const PORT = 8000;
 const main = async () => {
 	try {
 		const orm = await MikroORM.init(mikroOrmConfig);
 		await orm.getMigrator().up(); //Runs the migrations before it does anything
 
-		const ormFork = orm.em.fork();
-		const post = ormFork.create(Post, {
-			title: 'first posts',
-			createdAt: new Date(),
-			updatedAt: new Date(),
+		const app = express();
+
+		app.get('/', (_, res) => {
+			res.send('hello');
+		});
+
+		app.listen(PORT, () => {
+			console.log(`Serer started and listing on port: ${PORT}`);
 		});
 	} catch (error) {
 		console.log(error);
