beacffe181a945f78b83daf31ac254e2f67838c0
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 16:07:29 -0700

    name change

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,9 +1,14 @@
 import { MikroORM } from '@mikro-orm/postgresql';
+import path from 'path';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 
 export default {
-	dbName: 'full-typescript-project',
+	migrations: {
+		path: path.join(__dirname, './migrations'), // path to the folder with migrations,
+		glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
+	},
+	dbName: 'forum-clone',
 	entities: [Post],
 	debug: !__prod__,
 	type: 'postgresql',
