0d2c9cf8f01cb75fe89d714e731838b99726c808
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 16:32:04 -0700

    typegraphql server set up

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import mikroOrmConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const PORT = 8000;
 const main = async () => {
@@ -11,9 +14,15 @@ const main = async () => {
 
 		const app = express();
 
-		app.get('/', (_, res) => {
-			res.send('hello');
+		const apolloServer = new ApolloServer({
+			schema: await buildSchema({
+				resolvers: [HelloResolver],
+				validate: false,
+			}),
 		});
+
+		await apolloServer.start();
+		apolloServer.applyMiddleware({ app });
 
 		app.listen(PORT, () => {
 			console.log(`Serer started and listing on port: ${PORT}`);
