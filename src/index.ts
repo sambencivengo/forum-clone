ecfab27e623bc8e0ee6501d994ca6293a5846013
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-16 11:00:41 -0700

    hashing password with argon2 and persiting and saving user

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import mikroOrmConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { HelloResolver, PostResolver } from './resolvers';
+import { HelloResolver, PostResolver, UserResolver } from './resolvers';
 
 const PORT = 8000;
 const main = async () => {
@@ -17,7 +17,7 @@ const main = async () => {
 
 		const apolloServer = new ApolloServer({
 			schema: await buildSchema({
-				resolvers: [HelloResolver, PostResolver],
+				resolvers: [HelloResolver, PostResolver, UserResolver],
 				validate: false,
 			}),
 			context: () => ({ em: orm.em }), // Special object that is accessible by all of your resolvers
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,7 @@
 import { User } from '../entities';
 import { EntityManagerType } from 'src/types';
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
-
+import argon2 from 'argon2';
 // Alternative way to use @Arg decorators for arguments
 @InputType()
 class UsernamePasswordInput {
@@ -13,14 +13,16 @@ class UsernamePasswordInput {
 
 @Resolver()
 export class UserResolver {
-	@Mutation(() => String)
-	register(
+	@Mutation(() => User)
+	async register(
 		@Arg('options') options: UsernamePasswordInput,
 		@Ctx() { em }: EntityManagerType
 	) {
 		const { password, username } = options;
-		const hashedPassword = password;
-		// Hash password
-		em.create(User, { username, password: hashedPassword });
+		const hashedPassword = await argon2.hash(password); // Argon2 hashed password
+
+		const user = em.create(User, { username, password: hashedPassword });
+		await em.persistAndFlush(user);
+		return user;
 	}
 }
