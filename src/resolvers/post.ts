c93fd5dce6f50622d5f6887b1678a962453bf546
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 17:34:17 -0700

    graphql findOne post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from '../entities';
 import { MyContext } from 'src/types';
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
 	@Query(() => [Post]) // Arrays in GraphQL are an array with the type inside
-	post(@Ctx() { em }: MyContext): Promise<Post[]> {
+	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
 		return em.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true }) // Arrays in GraphQL are an array with the type inside
+	post(
+		@Arg('id', () => Int) id: number,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		return em.findOne(Post, { id });
 	}
 }
