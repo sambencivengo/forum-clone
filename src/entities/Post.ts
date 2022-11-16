a1c997fbf9e2f403655cdbee5af945c926bc562d
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 18:00:01 -0700

    fixed typing for Post entity and added a graphql resolver for creating a post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,22 +1,32 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, OptionalProps, PrimaryKey, Property } from '@mikro-orm/core';
 import { Field, Int, ObjectType } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class Post {
+	[OptionalProps]?: 'updatedAt' | 'createdAt';
+
 	@Field(() => Int) // Exposes to the GraphQL Schema
 	@PrimaryKey()
 	id!: number;
 
 	@Field(() => Date)
-	@Property({ type: Date })
+	@Property({ type: Date, defaultRaw: 'clock_timestamp()' })
 	createdAt = new Date();
 
 	@Field(() => Date)
-	@Property({ type: Date, onUpdate: () => new Date() })
+	@Property({
+		type: Date,
+		defaultRaw: 'clock_timestamp()',
+		onUpdate: () => new Date(),
+	})
 	updatedAt = new Date();
 
 	@Field()
 	@Property({ type: 'text' })
 	title!: string;
+
+	constructor(title: string) {
+		this.title = title;
+	}
 }
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from '../entities';
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
@@ -11,9 +11,18 @@ export class PostResolver {
 
 	@Query(() => Post, { nullable: true }) // Arrays in GraphQL are an array with the type inside
 	post(
-		@Arg('id', () => Int) id: number,
+		@Arg('id') id: number,
 		@Ctx() { em }: MyContext
 	): Promise<Post | null> {
 		return em.findOne(Post, { id });
+	}
+
+	@Mutation(() => Post) // Arrays in GraphQL are an array with the type inside
+	async createPost(
+		@Arg('title') title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post> {
+		const post = em.create(Post, { title });
+		return post;
 	}
 }
