d681917ef705569ded8780c9b18a61d09de2d7cd
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 21:13:28 -0700

    leftover

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
 
-export type MyContext = {
+export type EntityManagerTYPE = {
 	em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
 };
