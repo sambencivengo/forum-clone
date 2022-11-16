3e4f4ef9831f106e147e6529b1ec3777f97d260d
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-15 20:58:52 -0700

    'put' set up with resolvers

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -9,7 +9,7 @@ export class PostResolver {
 		return em.find(Post, {});
 	}
 
-	@Query(() => Post, { nullable: true }) // Arrays in GraphQL are an array with the type inside
+	@Query(() => Post, { nullable: true })
 	post(
 		@Arg('id') id: number,
 		@Ctx() { em }: MyContext
@@ -17,12 +17,30 @@ export class PostResolver {
 		return em.findOne(Post, { id });
 	}
 
-	@Mutation(() => Post) // Arrays in GraphQL are an array with the type inside
+	// without post and flush, the browser graphql call will error out due to returning NULL
+	@Mutation(() => Post)
 	async createPost(
 		@Arg('title') title: string,
 		@Ctx() { em }: MyContext
 	): Promise<Post> {
 		const post = em.create(Post, { title });
+		await em.persistAndFlush(post); // just like a controller and route, this will post and flush in order to save to the DB
+		return post;
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg('id') id: number, // Any other args will be a second @Arg call
+		@Arg('title', () => String, { nullable: true }) title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { id });
+		if (!post) {
+			return null;
+		}
+
+		if (title) post.title = title;
+		await em.persistAndFlush(post);
 		return post;
 	}
 }
